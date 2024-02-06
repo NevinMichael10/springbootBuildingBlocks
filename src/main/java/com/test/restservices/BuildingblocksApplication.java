4e24db4359e0fdf01058794af3af46c473b131e4
NevinMichael10 <nevinmichael28@gmail.com>
2024-02-06 14:34:20 +0530

    initialise

---
+++ src/main/java/com/test/restservices/BuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.test.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/restservices/BuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
